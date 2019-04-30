669d503e33993a3fc160865a77d8eed7af11aa76
ys <findsomeoneyysxxx@gmail.com>
2019-04-30 15:18:19 +0800

    init

---
+++ src/main/java/com/ys/egou/EgouApplication.java
@@ -0,0 +1,13 @@
+package com.ys.egou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EgouApplication.class, args);
+    }
+
+}
